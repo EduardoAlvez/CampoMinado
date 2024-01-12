cc9daf7a4d32f9010b93a86ad6af253212428529
Eduardo Alves <111307180+EduardoAlvez@users.noreply.github.com>
2024-01-12 10:21:28 -0300

    teste

--- CampoMinado/teste/br/com/cod3r/cm/modelo/CampoTest.java
+++ CampoMinado/teste/br/com/cod3r/cm/modelo/CampoTest.java
@@ -1,6 +1,6 @@
 package br.com.cod3r.cm.modelo;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
